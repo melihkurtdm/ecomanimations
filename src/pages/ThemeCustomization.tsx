8f976b0c9e741c85b54c1568c99f5f40e842fc05
Melih Kurt <melihkurt1134@gmail.com>
2025-05-23 13:45:56 +0300

    update

--- src/pages/ThemeCustomization.tsx
+++ src/pages/ThemeCustomization.tsx
@@ -64,8 +64,7 @@ const themeMap: Record<string, React.LazyExoticComponent<React.ComponentType<any
   minimalist: lazy(() => import("../themes/minimalist/ThemeLayout")),
   elegant: lazy(() => import("../themes/elegant/ThemeLayout")),
   "luxe-aura": lazy(() => import("../themes/luxe-aura/ThemeLayout")),
-  // Temporarily comment out themes until modules are available
-   "diamond-luxe": lazy(() => import("../themes/diamond-luxe/ThemeLayout")),
+  "diamond-luxe": lazy(() => import("../themes/diamond-luxe/ThemeLayout")),
   "modern": lazy(() => import("../themes/modern/ThemeLayout"))
 };
 
