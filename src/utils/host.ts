dd2a43ae430cbd427414b6bebfd86e4e2300679a
melihkurtdm <melihkurt1134@gmail.com>
2026-02-22 20:54:31 +0300

    normalize domain host

--- src/utils/host.ts
+++ src/utils/host.ts
@@ -1,4 +1,3 @@
-// src/utils/host.ts
-export function getHostNoWww() {
-    return window.location.hostname.replace(/^www\./, "").toLowerCase();
+export function getHostNow() {
+    return window.location.hostname.replace(/^www\./, "");
   }
