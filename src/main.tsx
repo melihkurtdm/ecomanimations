8ef1076d026521a692406fd6009aaefd9138ea68
melihkurtdm <melihkurt1134@gmail.com>
2026-02-24 03:58:14 +0300

    remove unused ADMIN_HOSTS import

--- src/main.tsx
+++ src/main.tsx
@@ -6,25 +6,21 @@ import { ThemeProvider } from "./contexts/ThemeContext";
 import { AuthProvider } from "./contexts/AuthContext";
 
 import { getHostNow } from "./utils/host";
-import { ADMIN_HOSTS } from "./config/domains";
 
-import { adminRouter } from "./routes/adminRouter";
 import { storefrontRouter } from "./routes/storefrontRouter";
 
 const DEBUG = import.meta.env.DEV || import.meta.env.VITE_DEBUG_STORE === "1";
-
 const host = getHostNow();
-const isAdminHost = ADMIN_HOSTS.has(host);
 
 if (DEBUG) {
-  console.log("[ROUTER_RESOLVE]", { host, isAdminHost });
+  console.log("[ROUTER_RESOLVE]", { host });
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
       <ThemeProvider>
-        <RouterProvider router={isAdminHost ? adminRouter : storefrontRouter} />
+        <RouterProvider router={storefrontRouter} />  
       </ThemeProvider>
     </AuthProvider>
   </React.StrictMode>
